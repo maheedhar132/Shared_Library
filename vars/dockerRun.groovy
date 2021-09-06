b1dcbeadb33b6087d596bb5c7a3022d3e8b8f8f2
maheedhar132 <45238528+maheedhar132@users.noreply.github.com>
2021-09-06 16:47:37 +0530

    Update dockerRun.groovy

--- vars/dockerRun.groovy
+++ vars/dockerRun.groovy
@@ -1,8 +1,12 @@
 def call(body){
     def config = [:]
-    body.resolveStrategy = Closure.DELEGATE_FIRST
-    body.delegate = config
-    body()
+	body.resolveStrategy = Closure.DELEGATE_FIRST
+	body.delegate = config
+	body()
+    print(1.**)
+    print(config.dockerImageName)
+    print(2.****)
+    print(config.dockerImage)
     def dockerImageName = config.dockerImageName
     def dockerImage = """$dockerImageName"""
     print(dockerImageName)
